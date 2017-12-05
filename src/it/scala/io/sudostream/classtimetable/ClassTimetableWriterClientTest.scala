c9ec3c535f92d655335f1a75160387df8b999571
Andy Boyle <andy@sudostream.io>
2017-12-05 12:43:04 +0000

    0.0.1-2 : Setting up integration tests

---
+++ src/it/scala/io/sudostream/classtimetable/ClassTimetableWriterClientTest.scala
@@ -0,0 +1,12 @@
+package io.sudostream.classtimetable
+
+import org.scalatest._
+
+class ClassTimetableWriterClientTest extends FlatSpec with Matchers {
+  println("ClassTimetableWriter Client Tester")
+
+  "My code" should "work" in {
+    true should be(true)
+  }
+
+}
